1ce018412edd375e9b737dc36234bdd9ca9e03ca
Magomogo <mgnatenko@gmail.com>
2016-07-12 13:21:43 +0200

    add test for get() method

--- tests/integration/GetTest.php
+++ tests/integration/GetTest.php
@@ -2,8 +2,44 @@
 
 class GetTest extends PHPUnit_Framework_TestCase
 {
-    public function testNotExistingContent()
+    /**
+     * @var \Barberry\Client
+     */
+    private $client;
+
+    public function setUp()
     {
-        var_dump(getenv('BARBERRY'));
+        $this->client = new Barberry\Client(getenv('BARBERRY'));
+    }
+
+    public function testNotExistingContentCausesException()
+    {
+        $this->setExpectedException('Barberry\\Exception');
+        $this->client->get('not-existing');
+    }
+
+    public function testExistingContent()
+    {
+        $id = self::uploadImage(__DIR__ . '/data/image.jpg');
+
+        $this->assertEquals(
+            file_get_contents(__DIR__ . '/data/image.jpg'),
+            $this->client->get($id)
+        );
+    }
+
+    private static function uploadImage($filePath)
+    {
+        $guzzle = new \GuzzleHttp\Client();
+        $response = $guzzle->post('http://' . getenv('BARBERRY') . '/', array(
+            'body' => array(
+                'field_name'     => 'file',
+                'file_filed' => fopen($filePath, 'r')
+            )
+        ));
+
+        $metaInfo = $response->json();
+
+        return $metaInfo['id'];
     }
 }
