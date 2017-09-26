83a48d02df3b4819ecec9d3a7f498aecf4c878e1
Vladislav Vasinkin <vvasinkin@xiag.ch>
2017-09-26 12:13:19 +0700

    Made barberry/client to repeat request on network failure

--- tests/integration/GetTest.php
+++ tests/integration/GetTest.php
@@ -15,7 +15,7 @@ class GetTest extends PHPUnit_Framework_TestCase
     public function testNotExistingContentCausesException()
     {
         $this->setExpectedException('Barberry\\Exception');
-        $this->client->get('not-existing');
+        $this->client->get(getenv('BARBERRY') . '/not-existing');
     }
 
     public function testExistingContent()
@@ -26,6 +26,17 @@ class GetTest extends PHPUnit_Framework_TestCase
             file_get_contents(__DIR__ . '/data/image.jpg'),
             $this->client->get($id)
         );
+    }
+
+    public function testUnavailableService()
+    {
+        $client = new Barberry\Client('127.0.0.1', 300, 50);
+        try {
+            $client->get('service-unavailable');
+        } catch (Barberry\Exception $e) {
+            $this->assertSame('Barberry service temporary unavailable', $e->getMessage());
+        }
+        $this->assertTrue($client->getRetries() === 0);
     }
 
     private static function uploadImage($filePath)
